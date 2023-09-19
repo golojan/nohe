d22b795dcdb48bb2109fb90f43ca5a290b85012e
Agu Chux <agu.chux@yahoo.com>
2023-09-19 03:00:38 +0100

    Our doctors fix record

--- libs/models/src/doctors.model.ts
+++ libs/models/src/doctors.model.ts
@@ -1,87 +1,88 @@
-import mongoose, { Document } from 'mongoose'
+import mongoose, { Document } from "mongoose";
 
-mongoose.Promise = global.Promise
+mongoose.Promise = global.Promise;
 
 const doctorsScheme = new mongoose.Schema(
-    {
-        username: {
-            type: String,
-            required: true,
-            unique: true,
-        },
-        picture: {
-            type: String,
-            default: '/images/avatar/user.png',
-        },
-        firstname: {
-            type: String,
-            default: '',
-        },
-        lastname: {
-            type: String,
-            default: '',
-        },
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-        },
-        mobile: String,
-        enable_otp: String,
-        otp: String,
-        password: String,
-        gender: {
-            type: String,
-        },
-        birthday: { type: String, default: '' },
-        department: {
-            type: String,
-            default: 'General',
-        },
-        unit: {
-            type: String,
-            default: 'Clinic',
-        },
-        role: {
-            type: String,
-            default: 'Doctor',
-        },
-        smsNotification: {
-            type: Boolean,
-            default: false,
-        },
-        emailNotification: {
-            type: Boolean,
-            default: false,
-        },
-        smsBalance: {
-            type: Number,
-            default: 0,
-        },
-        lastLogin: {
-            type: Date,
-        },
-        delete: {
-            type: Boolean,
-            default: false,
-        },
-        deleteAt: {
-            type: Date,
-        },
-        disabled: {
-            type: Boolean,
-            default: false,
-        },
-        disableAt: {
-            type: Date,
-        },
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    { timestamps: true }
-)
+    picture: {
+      type: String,
+      default: "/assets/images/avatar.png",
+    },
+    firstname: {
+      type: String,
+      default: "",
+    },
+    lastname: {
+      type: String,
+      default: "",
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    mobile: String,
+    enable_otp: String,
+    otp: String,
+    password: String,
+    gender: {
+      type: String,
+    },
+    birthday: { type: String, default: "" },
+    department: {
+      type: String,
+      default: "General",
+    },
+    unit: {
+      type: String,
+      default: "Clinic",
+    },
+    role: {
+      type: String,
+      default: "Doctor",
+    },
+    smsNotification: {
+      type: Boolean,
+      default: false,
+    },
+    emailNotification: {
+      type: Boolean,
+      default: false,
+    },
+    smsBalance: {
+      type: Number,
+      default: 0,
+    },
+    lastLogin: {
+      type: Date,
+    },
+    delete: {
+      type: Boolean,
+      default: false,
+    },
+    deleteAt: {
+      type: Date,
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    disableAt: {
+      type: Date,
+    },
+  },
+  { timestamps: true }
+);
 
 if (mongoose.models.Doctors) {
-    delete mongoose.models.Doctors
+  delete mongoose.models.Doctors;
 }
 
-const Doctors = mongoose.models.Doctors || mongoose.model('Doctors', doctorsScheme)
-export default Doctors
+const Doctors =
+  mongoose.models.Doctors || mongoose.model("Doctors", doctorsScheme);
+export default Doctors;
